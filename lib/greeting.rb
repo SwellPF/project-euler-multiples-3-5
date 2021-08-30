a78952fa30743d775a7cf4bc6dc7ad54603f6d7b
Paul Fell <swellpf@gmail.com>
2021-08-30 16:23:03 -0400

    commit tree 1f440985ac217ebb98bab9b245cc2719607210fa

---
+++ lib/greeting.rb
@@ -0,0 +1,15 @@
+# def greeting(name='somebody')
+#     puts "Hello world! This is #{name}!"
+# end
+
+# greeting('Paul Cinoman')
+# greeting('Andy Cinoman')
+# greeting('Ginger Cinoman')
+# greeting
+
+
+def return_name
+    "Guy Fieri"
+end
+
+return_name
--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,27 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+    i = 1
+    perfectNums = Array.new
+    while i < num do
+        if i.modulo(3) == 0 || i.modulo(5) == 0
+            perfectNums << i
+        end
+    i += 1
+    end
+    return perfectNums
+end
+
+def sum_multiples(num)
+    i = 1
+    perfectSums = 0;
+    while i < num do
+        if i.modulo(3) == 0 || i.modulo(5) == 0
+            perfectSums += i
+        end
+    i += 1
+    end
+    return perfectSums
+end
+
+collect_multiples(10)
+
